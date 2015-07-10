e285ee87da9ed1a03cb6043bbdfa102d724b0db9
Mikko Perttunen <mperttunen@nvidia.com>
2015-07-10 13:55:00 +0300

    Add submitvic test to DRM tests
    This test tests the VIC and Host1x submissions.

--- tests/drm.py
+++ tests/drm.py
@@ -4,6 +4,7 @@ import os
 import pyudev
 import sys
 import unittest
+import subprocess
 
 import linux.drm
 
@@ -34,3 +35,13 @@ class drm(unittest.TestCase):
                                                     version.minor,
                                                     version.patch,
                                                     version.date))
+
+submit_vic_path = '/root/drm/tests/tegra/submit_vic'
+def submit_vic_exists():
+    return os.path.exists(submit_vic_path)
+
+@unittest.skipUnless(submit_vic_exists(), 'submit_vic DRM test not present')
+class submitvic(unittest.TestCase):
+    def test(self):
+        subprocess.check_call(submit_vic_path, stdout = subprocess.DEVNULL,
+                              stderr = subprocess.DEVNULL)
