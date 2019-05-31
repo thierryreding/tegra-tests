cced0e2d415005eed1ebc94479643a55d80ccb10
Thierry Reding <treding@nvidia.com>
2019-05-31 15:16:17 +0200

    linux: util: Track messages matched by whitelist entries
    Track which messages in the kernel log each entry in the whitelist
    matched. This can be used, for example, to determine which entries
    in the whitelist are stale and could be removed.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- linux/util.py
+++ linux/util.py
@@ -3,12 +3,32 @@
 import re
 
 class WhiteList():
+    class Pattern:
+        def __init__(self, re):
+            self.matches = []
+            self.re = re
+
     def __init__(self, patterns):
-        self.patterns = [ re.compile(pattern) for pattern in patterns ]
+        self.patterns = []
+
+        for pattern in patterns:
+            pattern = re.compile(pattern)
+            pattern = WhiteList.Pattern(pattern)
+            self.patterns.append(pattern)
+
+    def unmatched(self):
+        result = []
+
+        for pattern in self.patterns:
+            if not pattern.matches:
+                result.append(pattern.re.pattern)
+
+        return result
 
     def __contains__(self, item):
         for pattern in self.patterns:
-            if pattern.match(item.message):
+            if pattern.re.match(item.message):
+                pattern.matches.append(item.message)
                 return True
 
         return False
