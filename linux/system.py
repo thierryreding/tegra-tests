26c0dbc5a2961d522ef8506b1c8653d48c2ac416
Thierry Reding <treding@nvidia.com>
2019-06-06 17:56:20 +0200

    linux: system: Add ID EEPROM support
    This adds a class that can read the contents of an ID EEPROM from sysfs
    and parse it into a data structure.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- linux/system.py
+++ linux/system.py
@@ -281,3 +281,128 @@ class Watchdog():
         options = ctypes.pointer(ctypes.c_uint(Watchdog.WDIOS_DISABLECARD))
         libc.ioctl(self.fd, Watchdog.WDIOC_SETOPTIONS, options)
         os.close(self.fd)
+
+class UnknownVersion(Exception):
+    def __init__(self, version):
+        super().__init__('unknown version %s' % version)
+
+class EEPROM():
+    class MACAddress():
+        def __init__(self, data):
+            self.data = data
+
+        def __str__(self):
+            return ':'.join('%02x' % byte for byte in self.data)
+
+    class ConfigurationBlock():
+        def __init__(self, data):
+            self.signature = data[0:4].decode('ascii')
+            self.length = data[5] << 8 | data[4]
+            self.type = data[6:8].decode('ascii')
+            self.version = data[9] << 8 | data[8]
+
+            self.mac = {
+                    'wifi': EEPROM.MACAddress(data[15:9:-1]),
+                    'bluetooth': EEPROM.MACAddress(data[21:15:-1]),
+                    'ethernet': EEPROM.MACAddress(data[27:21:-1])
+                }
+
+        def dump(self, fobj = sys.stdout):
+            print('NV Configuration Block:', file = fobj)
+            print('  Signature: %s' % self.signature, file = fobj)
+            print('  Length: %u' % self.length, file = fobj)
+            print('  Type: %s' % self.type, file = fobj)
+            print('  Version: %04x' % self.version, file = fobj)
+            print('  MAC addresses:', file = fobj)
+            print('    WiFi: %s' % self.mac['wifi'], file = fobj)
+            print('    Bluetooth: %s' % self.mac['bluetooth'], file = fobj)
+            print('    Ethernet: %s' % self.mac['ethernet'], file = fobj)
+
+    def __init__(self, bus, name):
+        self.sysfs = sysfs.Object(os.path.join('bus', bus, 'devices'), name)
+        self.nvcb = None
+
+        with self.sysfs.open('eeprom', 'rb') as eeprom:
+            data = eeprom.read(-1)
+
+        if data[1] != 0 or data[0] != 1:
+            raise UnknownVersion('%04x' % (data[1] << 8 | data[0]))
+
+        self.version = data[1] << 8 | data[0]
+        self.size = data[3] << 8 | data[2]
+        self.board = data[5] << 8 | data[4]
+        self.sku = data[7] << 8 | data[6]
+        self.fab = data[8]
+        self.rev = chr(data[9])
+        self.minor = data[10]
+
+        self.memory = data[11]
+        self.power = data[12]
+        self.misc = data[13]
+        self.modem = data[14]
+        self.touchscreen = data[15]
+        self.display = data[16]
+        self.rework = data[17]
+        self.sno = data[19] << 8 | data[18]
+        self.product = data[20:50].decode('ascii')
+
+        self.mac = {
+                'wifi': EEPROM.MACAddress(data[55:49:-1]),
+                'bluetooth': EEPROM.MACAddress(data[61:55:-1]),
+                'wifi_secondary': EEPROM.MACAddress(data[67:61:-1]),
+                'ethernet': EEPROM.MACAddress(data[73:67:-1])
+            }
+
+        self.serial = data[74:89].decode('ascii')
+
+        if data[150:154] == b'NVCB':
+            self.nvcb = EEPROM.ConfigurationBlock(data[150:255])
+
+        checksum = EEPROM.checksum(data[0:255])
+        crc = data[255]
+
+        if crc != checksum:
+            print('checksum mismatch: %02x, should be %02x' % (checksum, crc))
+
+    def dump(self, fobj = sys.stdout):
+        print('Version: %04x' % self.version, file = fobj)
+        print('Size: %u' % self.size, file = fobj)
+        print('Board: %u' % self.board, file = fobj)
+        print('SKU: %u' % self.sku, file = fobj)
+        print('Fab: %u' % self.fab, file = fobj)
+        print('Revision: %s' % self.rev, file = fobj)
+        print('Minor: %u' % self.minor, file = fobj)
+        print('Memory: %u' % self.memory, file = fobj)
+        print('Power: %u' % self.power, file = fobj)
+        print('Misc: %u' % self.misc, file = fobj)
+        print('Modem: %u' % self.modem, file = fobj)
+        print('Touchscreen: %u' % self.touchscreen, file = fobj)
+        print('Display: %u' % self.display, file = fobj)
+        print('Rework: %u' % self.rework, file = fobj)
+        print('SNO: %u' % self.sno, file = fobj)
+        print('Product: %s' % self.product, file = fobj)
+        print('MAC addresses:', file = fobj)
+        print('  WiFi: %s' % self.mac['wifi'], file = fobj)
+        print('  Bluetooth: %s' % self.mac['bluetooth'], file = fobj)
+        print('  Secondary WiFi: %s' % self.mac['wifi_secondary'], file = fobj)
+        print('  Ethernet: %s' % self.mac['ethernet'], file = fobj)
+        print('Serial: %s' % self.serial, file = fobj)
+
+        if self.nvcb:
+            self.nvcb.dump(fobj)
+
+    @staticmethod
+    def checksum(data):
+        crc = 0
+
+        for byte in data:
+            for i in range(8):
+                odd = ((crc ^ byte) & 1) == 1
+
+                byte >>= 1
+                crc >>= 1
+
+                if odd:
+                    crc ^= 0x8c
+
+        return crc & 0xff
