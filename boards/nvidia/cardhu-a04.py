4320f40970496056f177b0c370c62a35aad8cff4
Thierry Reding <treding@nvidia.com>
2022-09-14 16:08:56 +0200

    boards: Add spaces after =
    These have been missing since the original addition, most likely due to
    a botched mass reformatting operation.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- boards/nvidia/cardhu-a04.py
+++ boards/nvidia/cardhu-a04.py
@@ -8,61 +8,61 @@ class Board(boards.Board):
 
     devices = [
         # platform bus
-        sysfs.Device(bus = 'platform', name = '3000.pcie', driver ='tegra-pcie'),
-        sysfs.Device(bus = 'platform', name = '50000000.host1x', driver ='tegra-host1x'),
-        sysfs.Device(bus = 'platform', name = '54140000.gr2d', driver ='tegra-gr2d'),
-        sysfs.Device(bus = 'platform', name = '60005000.timer', driver ='tegra-wdt'),
-        sysfs.Device(bus = 'platform', name = '60007000.flow-controller', driver ='tegra-flowctrl'),
-        sysfs.Device(bus = 'platform', name = '6000a000.dma', driver ='tegra-apbdma'),
-        sysfs.Device(bus = 'platform', name = '6000c000.ahb', driver ='tegra-ahb'),
-        sysfs.Device(bus = 'platform', name = '6000d000.gpio', driver ='tegra-gpio'),
-        sysfs.Device(bus = 'platform', name = '70000868.pinmux', driver ='tegra30-pinctrl'),
+        sysfs.Device(bus = 'platform', name = '3000.pcie', driver = 'tegra-pcie'),
+        sysfs.Device(bus = 'platform', name = '50000000.host1x', driver = 'tegra-host1x'),
+        sysfs.Device(bus = 'platform', name = '54140000.gr2d', driver = 'tegra-gr2d'),
+        sysfs.Device(bus = 'platform', name = '60005000.timer', driver = 'tegra-wdt'),
+        sysfs.Device(bus = 'platform', name = '60007000.flow-controller', driver = 'tegra-flowctrl'),
+        sysfs.Device(bus = 'platform', name = '6000a000.dma', driver = 'tegra-apbdma'),
+        sysfs.Device(bus = 'platform', name = '6000c000.ahb', driver = 'tegra-ahb'),
+        sysfs.Device(bus = 'platform', name = '6000d000.gpio', driver = 'tegra-gpio'),
+        sysfs.Device(bus = 'platform', name = '70000868.pinmux', driver = 'tegra30-pinctrl'),
         sysfs.Device(bus = 'platform', name = '70006000.serial', driver = [ 'of_serial', 'tegra-uart' ]),
-        sysfs.Device(bus = 'platform', name = '70006200.serial', driver ='serial-tegra'),
-        sysfs.Device(bus = 'platform', name = '7000a000.pwm', driver ='tegra-pwm'),
-        sysfs.Device(bus = 'platform', name = '7000c000.i2c', driver ='tegra-i2c'),
-        sysfs.Device(bus = 'platform', name = '7000c400.i2c', driver ='tegra-i2c'),
-        sysfs.Device(bus = 'platform', name = '7000c500.i2c', driver ='tegra-i2c'),
-        sysfs.Device(bus = 'platform', name = '7000c700.i2c', driver ='tegra-i2c'),
-        sysfs.Device(bus = 'platform', name = '7000d000.i2c', driver ='tegra-i2c'),
-        sysfs.Device(bus = 'platform', name = '7000da00.spi', driver ='spi-tegra-slink'),
-        sysfs.Device(bus = 'platform', name = '7000e000.rtc', driver ='tegra_rtc'),
-        sysfs.Device(bus = 'platform', name = '7000e400.pmc', driver ='tegra-pmc'),
-        sysfs.Device(bus = 'platform', name = '7000f000.memory-controller', driver ='tegra-mc'),
-        sysfs.Device(bus = 'platform', name = '7000f400.memory-controller', driver ='tegra30-emc'),
-        sysfs.Device(bus = 'platform', name = '7000f800.fuse', driver ='tegra-fuse'),
-        sysfs.Device(bus = 'platform', name = '70080000.ahub', driver ='tegra30-ahub'),
-        sysfs.Device(bus = 'platform', name = '70080400.i2s', driver ='tegra30-i2s'),
-        sysfs.Device(bus = 'platform', name = '7d008000.usb', driver ='tegra-usb'),
-        sysfs.Device(bus = 'platform', name = '7d008000.usb-phy', driver ='tegra-phy'),
-        sysfs.Device(bus = 'platform', name = 'gpio-keys', driver ='gpio-keys'),
-        sysfs.Device(bus = 'platform', name = 'pmu', driver ='armv7-pmu'),
-        sysfs.Device(bus = 'platform', name = 'tps65910-gpio', driver ='tps65910-gpio'),
-        sysfs.Device(bus = 'platform', name = 'tps65910-pmic', driver ='tps65910-pmic'),
-        sysfs.Device(bus = 'platform', name = 'tps65910-rtc', driver ='tps65910-rtc'),
+        sysfs.Device(bus = 'platform', name = '70006200.serial', driver = 'serial-tegra'),
+        sysfs.Device(bus = 'platform', name = '7000a000.pwm', driver = 'tegra-pwm'),
+        sysfs.Device(bus = 'platform', name = '7000c000.i2c', driver = 'tegra-i2c'),
+        sysfs.Device(bus = 'platform', name = '7000c400.i2c', driver = 'tegra-i2c'),
+        sysfs.Device(bus = 'platform', name = '7000c500.i2c', driver = 'tegra-i2c'),
+        sysfs.Device(bus = 'platform', name = '7000c700.i2c', driver = 'tegra-i2c'),
+        sysfs.Device(bus = 'platform', name = '7000d000.i2c', driver = 'tegra-i2c'),
+        sysfs.Device(bus = 'platform', name = '7000da00.spi', driver = 'spi-tegra-slink'),
+        sysfs.Device(bus = 'platform', name = '7000e000.rtc', driver = 'tegra_rtc'),
+        sysfs.Device(bus = 'platform', name = '7000e400.pmc', driver = 'tegra-pmc'),
+        sysfs.Device(bus = 'platform', name = '7000f000.memory-controller', driver = 'tegra-mc'),
+        sysfs.Device(bus = 'platform', name = '7000f400.memory-controller', driver = 'tegra30-emc'),
+        sysfs.Device(bus = 'platform', name = '7000f800.fuse', driver = 'tegra-fuse'),
+        sysfs.Device(bus = 'platform', name = '70080000.ahub', driver = 'tegra30-ahub'),
+        sysfs.Device(bus = 'platform', name = '70080400.i2s', driver = 'tegra30-i2s'),
+        sysfs.Device(bus = 'platform', name = '7d008000.usb', driver = 'tegra-usb'),
+        sysfs.Device(bus = 'platform', name = '7d008000.usb-phy', driver = 'tegra-phy'),
+        sysfs.Device(bus = 'platform', name = 'gpio-keys', driver = 'gpio-keys'),
+        sysfs.Device(bus = 'platform', name = 'pmu', driver = 'armv7-pmu'),
+        sysfs.Device(bus = 'platform', name = 'tps65910-gpio', driver = 'tps65910-gpio'),
+        sysfs.Device(bus = 'platform', name = 'tps65910-pmic', driver = 'tps65910-pmic'),
+        sysfs.Device(bus = 'platform', name = 'tps65910-rtc', driver = 'tps65910-rtc'),
     # Device trees in Linux v5.9 changed sdhci@... to mmc@... and iram@... to sram@...
     ] + [
         device for device in [
-            sysfs.Device(bus = 'platform', name = '40000000.iram', driver ='sram'),
-            sysfs.Device(bus = 'platform', name = '78000000.sdhci', driver ='sdhci-tegra'),
-            sysfs.Device(bus = 'platform', name = '78000400.sdhci', driver ='sdhci-tegra'),
-            sysfs.Device(bus = 'platform', name = '78000600.sdhci', driver ='sdhci-tegra'),
+            sysfs.Device(bus = 'platform', name = '40000000.iram', driver = 'sram'),
+            sysfs.Device(bus = 'platform', name = '78000000.sdhci', driver = 'sdhci-tegra'),
+            sysfs.Device(bus = 'platform', name = '78000400.sdhci', driver = 'sdhci-tegra'),
+            sysfs.Device(bus = 'platform', name = '78000600.sdhci', driver = 'sdhci-tegra'),
         ] if Kernel().version < Kernel.Version('5.9.0')
     ] + [
         device for device in [
-            sysfs.Device(bus = 'platform', name = '40000000.sram', driver ='sram'),
-            sysfs.Device(bus = 'platform', name = '78000000.mmc', driver ='sdhci-tegra'),
-            sysfs.Device(bus = 'platform', name = '78000400.mmc', driver ='sdhci-tegra'),
-            sysfs.Device(bus = 'platform', name = '78000600.mmc', driver ='sdhci-tegra'),
+            sysfs.Device(bus = 'platform', name = '40000000.sram', driver = 'sram'),
+            sysfs.Device(bus = 'platform', name = '78000000.mmc', driver = 'sdhci-tegra'),
+            sysfs.Device(bus = 'platform', name = '78000400.mmc', driver = 'sdhci-tegra'),
+            sysfs.Device(bus = 'platform', name = '78000600.mmc', driver = 'sdhci-tegra'),
         ] if Kernel().version >= Kernel.Version('5.9.0')
     # Linux v5.14 changed the sound card driver name from 'tegra-snd-wm8903' to 'tegra-wm8903'
     ] + [
         device for device in [
-            sysfs.Device(bus = 'platform', name = 'sound', driver ='tegra-snd-wm8903'),
+            sysfs.Device(bus = 'platform', name = 'sound', driver = 'tegra-snd-wm8903'),
         ] if Kernel().version < Kernel.Version('5.14.0')
     ] + [
         device for device in [
-            sysfs.Device(bus = 'platform', name = 'sound', driver ='tegra-wm8903'),
+            sysfs.Device(bus = 'platform', name = 'sound', driver = 'tegra-wm8903'),
         ] if Kernel().version >= Kernel.Version('5.14.0')
     ] + [
         device for device in [
@@ -79,15 +79,15 @@ class Board(boards.Board):
         ] if Kernel().version < Kernel.Version('5.19.0')
     # I2C bus
     ] + [
-        sysfs.Device(bus = 'i2c', name = '2-0044', driver ='isl29028'),
-        sysfs.Device(bus = 'i2c', name = '2-0070', driver ='pca954x'),
-        sysfs.Device(bus = 'i2c', name = '4-001a', driver ='wm8903'),
-        sysfs.Device(bus = 'i2c', name = '4-002d', driver ='tps65910'),
-        sysfs.Device(bus = 'i2c', name = '4-004c', driver ='lm90'),
-        sysfs.Device(bus = 'i2c', name = '4-0060', driver ='tps62360'),
+        sysfs.Device(bus = 'i2c', name = '2-0044', driver = 'isl29028'),
+        sysfs.Device(bus = 'i2c', name = '2-0070', driver = 'pca954x'),
+        sysfs.Device(bus = 'i2c', name = '4-001a', driver = 'wm8903'),
+        sysfs.Device(bus = 'i2c', name = '4-002d', driver = 'tps65910'),
+        sysfs.Device(bus = 'i2c', name = '4-004c', driver = 'lm90'),
+        sysfs.Device(bus = 'i2c', name = '4-0060', driver = 'tps62360'),
     # SPI bus
     ] + [
-        sysfs.Device(bus = 'spi', name = 'spi0.1', driver ='spi-nor'),
+        sysfs.Device(bus = 'spi', name = 'spi0.1', driver = 'spi-nor'),
     ]
 
     allowlist = [
--- boards/nvidia/ventana.py
+++ boards/nvidia/ventana.py
@@ -8,75 +8,75 @@ class Board(boards.Board):
 
     devices = [
         # platform bus
-        sysfs.Device(bus = 'platform', name = '50000000.host1x', driver ='tegra-host1x'),
-        sysfs.Device(bus = 'platform', name = '54140000.gr2d', driver ='tegra-gr2d'),
-        sysfs.Device(bus = 'platform', name = '54180000.gr3d', driver ='tegra-gr3d'),
-        sysfs.Device(bus = 'platform', name = '54200000.dc', driver ='tegra-dc'),
-        sysfs.Device(bus = 'platform', name = '54240000.dc', driver ='tegra-dc'),
-        sysfs.Device(bus = 'platform', name = '54280000.hdmi', driver ='tegra-hdmi'),
-        sysfs.Device(bus = 'platform', name = '60007000.flow-controller', driver ='tegra-flowctrl'),
-        sysfs.Device(bus = 'platform', name = '6000a000.dma', driver ='tegra-apbdma'),
-        sysfs.Device(bus = 'platform', name = '6000c000.ahb', driver ='tegra-ahb'),
-        sysfs.Device(bus = 'platform', name = '6000d000.gpio', driver ='tegra-gpio'),
-        sysfs.Device(bus = 'platform', name = '70000014.pinmux', driver ='tegra20-pinctrl'),
-        sysfs.Device(bus = 'platform', name = '70000c00.das', driver ='tegra20-das'),
-        sysfs.Device(bus = 'platform', name = '70002800.i2s', driver ='tegra20-i2s'),
+        sysfs.Device(bus = 'platform', name = '50000000.host1x', driver = 'tegra-host1x'),
+        sysfs.Device(bus = 'platform', name = '54140000.gr2d', driver = 'tegra-gr2d'),
+        sysfs.Device(bus = 'platform', name = '54180000.gr3d', driver = 'tegra-gr3d'),
+        sysfs.Device(bus = 'platform', name = '54200000.dc', driver = 'tegra-dc'),
+        sysfs.Device(bus = 'platform', name = '54240000.dc', driver = 'tegra-dc'),
+        sysfs.Device(bus = 'platform', name = '54280000.hdmi', driver = 'tegra-hdmi'),
+        sysfs.Device(bus = 'platform', name = '60007000.flow-controller', driver = 'tegra-flowctrl'),
+        sysfs.Device(bus = 'platform', name = '6000a000.dma', driver = 'tegra-apbdma'),
+        sysfs.Device(bus = 'platform', name = '6000c000.ahb', driver = 'tegra-ahb'),
+        sysfs.Device(bus = 'platform', name = '6000d000.gpio', driver = 'tegra-gpio'),
+        sysfs.Device(bus = 'platform', name = '70000014.pinmux', driver = 'tegra20-pinctrl'),
+        sysfs.Device(bus = 'platform', name = '70000c00.das', driver = 'tegra20-das'),
+        sysfs.Device(bus = 'platform', name = '70002800.i2s', driver = 'tegra20-i2s'),
         sysfs.Device(bus = 'platform', name = '70006300.serial', driver = [ 'of_serial', 'tegra-uart' ]),
-        sysfs.Device(bus = 'platform', name = '7000a000.pwm', driver ='tegra-pwm'),
-        sysfs.Device(bus = 'platform', name = '7000c000.i2c', driver ='tegra-i2c'),
-        sysfs.Device(bus = 'platform', name = '7000c400.i2c', driver ='tegra-i2c'),
-        sysfs.Device(bus = 'platform', name = '7000c500.i2c', driver ='tegra-i2c'),
-        sysfs.Device(bus = 'platform', name = '7000d000.i2c', driver ='tegra-i2c'),
-        sysfs.Device(bus = 'platform', name = '7000e000.rtc', driver ='tegra_rtc'),
-        sysfs.Device(bus = 'platform', name = '7000e400.pmc', driver ='tegra-pmc'),
-        sysfs.Device(bus = 'platform', name = '7000f000.memory-controller', driver ='tegra-mc'),
-        sysfs.Device(bus = 'platform', name = '7000f400.memory-controller', driver ='tegra20-emc'),
-        sysfs.Device(bus = 'platform', name = '7000f800.fuse', driver ='tegra-fuse'),
-        sysfs.Device(bus = 'platform', name = 'c5000000.usb', driver ='tegra-usb'),
-        sysfs.Device(bus = 'platform', name = 'c5000000.usb-phy', driver ='tegra-phy'),
-        sysfs.Device(bus = 'platform', name = 'c5004000.usb', driver ='tegra-usb'),
-        sysfs.Device(bus = 'platform', name = 'c5004000.usb-phy', driver ='tegra-phy'),
-        sysfs.Device(bus = 'platform', name = 'c5008000.usb', driver ='tegra-usb'),
-        sysfs.Device(bus = 'platform', name = 'c5008000.usb-phy', driver ='tegra-phy'),
-        sysfs.Device(bus = 'platform', name = 'gpio-keys', driver ='gpio-keys'),
-        sysfs.Device(bus = 'platform', name = 'pmu', driver ='armv7-pmu'),
-        sysfs.Device(bus = 'platform', name = 'tegra20-cpufreq', driver ='tegra20-cpufreq'),
-        sysfs.Device(bus = 'platform', name = 'tps6586x-gpio', driver ='tps6586x-gpio'),
-        sysfs.Device(bus = 'platform', name = 'tps6586x-regulator', driver ='tps6586x-regulator'),
-        sysfs.Device(bus = 'platform', name = 'tps6586x-rtc', driver ='tps6586x-rtc'),
+        sysfs.Device(bus = 'platform', name = '7000a000.pwm', driver = 'tegra-pwm'),
+        sysfs.Device(bus = 'platform', name = '7000c000.i2c', driver = 'tegra-i2c'),
+        sysfs.Device(bus = 'platform', name = '7000c400.i2c', driver = 'tegra-i2c'),
+        sysfs.Device(bus = 'platform', name = '7000c500.i2c', driver = 'tegra-i2c'),
+        sysfs.Device(bus = 'platform', name = '7000d000.i2c', driver = 'tegra-i2c'),
+        sysfs.Device(bus = 'platform', name = '7000e000.rtc', driver = 'tegra_rtc'),
+        sysfs.Device(bus = 'platform', name = '7000e400.pmc', driver = 'tegra-pmc'),
+        sysfs.Device(bus = 'platform', name = '7000f000.memory-controller', driver = 'tegra-mc'),
+        sysfs.Device(bus = 'platform', name = '7000f400.memory-controller', driver = 'tegra20-emc'),
+        sysfs.Device(bus = 'platform', name = '7000f800.fuse', driver = 'tegra-fuse'),
+        sysfs.Device(bus = 'platform', name = 'c5000000.usb', driver = 'tegra-usb'),
+        sysfs.Device(bus = 'platform', name = 'c5000000.usb-phy', driver = 'tegra-phy'),
+        sysfs.Device(bus = 'platform', name = 'c5004000.usb', driver = 'tegra-usb'),
+        sysfs.Device(bus = 'platform', name = 'c5004000.usb-phy', driver = 'tegra-phy'),
+        sysfs.Device(bus = 'platform', name = 'c5008000.usb', driver = 'tegra-usb'),
+        sysfs.Device(bus = 'platform', name = 'c5008000.usb-phy', driver = 'tegra-phy'),
+        sysfs.Device(bus = 'platform', name = 'gpio-keys', driver = 'gpio-keys'),
+        sysfs.Device(bus = 'platform', name = 'pmu', driver = 'armv7-pmu'),
+        sysfs.Device(bus = 'platform', name = 'tegra20-cpufreq', driver = 'tegra20-cpufreq'),
+        sysfs.Device(bus = 'platform', name = 'tps6586x-gpio', driver = 'tps6586x-gpio'),
+        sysfs.Device(bus = 'platform', name = 'tps6586x-regulator', driver = 'tps6586x-regulator'),
+        sysfs.Device(bus = 'platform', name = 'tps6586x-rtc', driver = 'tps6586x-rtc'),
     # Device trees in Linux v5.9 changed sdhci@... to mmc@... and iram@... to sram@...
     ] + [
         device for device in [
-            sysfs.Device(bus = 'platform', name = '40000000.iram', driver ='sram'),
-            sysfs.Device(bus = 'platform', name = 'c8000000.sdhci', driver ='sdhci-tegra'),
-            sysfs.Device(bus = 'platform', name = 'c8000400.sdhci', driver ='sdhci-tegra'),
-            sysfs.Device(bus = 'platform', name = 'c8000600.sdhci', driver ='sdhci-tegra'),
+            sysfs.Device(bus = 'platform', name = '40000000.iram', driver = 'sram'),
+            sysfs.Device(bus = 'platform', name = 'c8000000.sdhci', driver = 'sdhci-tegra'),
+            sysfs.Device(bus = 'platform', name = 'c8000400.sdhci', driver = 'sdhci-tegra'),
+            sysfs.Device(bus = 'platform', name = 'c8000600.sdhci', driver = 'sdhci-tegra'),
         ] if Kernel().version < Kernel.Version('5.9.0')
     ] + [
         device for device in [
-            sysfs.Device(bus = 'platform', name = '40000000.sram', driver ='sram'),
-            sysfs.Device(bus = 'platform', name = 'c8000000.mmc', driver ='sdhci-tegra'),
-            sysfs.Device(bus = 'platform', name = 'c8000400.mmc', driver ='sdhci-tegra'),
-            sysfs.Device(bus = 'platform', name = 'c8000600.mmc', driver ='sdhci-tegra'),
+            sysfs.Device(bus = 'platform', name = '40000000.sram', driver = 'sram'),
+            sysfs.Device(bus = 'platform', name = 'c8000000.mmc', driver = 'sdhci-tegra'),
+            sysfs.Device(bus = 'platform', name = 'c8000400.mmc', driver = 'sdhci-tegra'),
+            sysfs.Device(bus = 'platform', name = 'c8000600.mmc', driver = 'sdhci-tegra'),
         ] if Kernel().version >= Kernel.Version('5.9.0')
     # Linux v5.14 changed the sound card driver name from 'tegra-snd-wm8903' to 'tegra-wm8903'
     ] + [
         device for device in [
-            sysfs.Device(bus = 'platform', name = 'sound', driver ='tegra-snd-wm8903'),
+            sysfs.Device(bus = 'platform', name = 'sound', driver = 'tegra-snd-wm8903'),
         ] if Kernel().version < Kernel.Version('5.14.0')
     ] + [
         device for device in [
-            sysfs.Device(bus = 'platform', name = 'sound', driver ='tegra-wm8903'),
+            sysfs.Device(bus = 'platform', name = 'sound', driver = 'tegra-wm8903'),
         ] if Kernel().version >= Kernel.Version('5.14.0')
     # host1x bus
     ] + [
-        sysfs.Device(bus = 'host1x', name = 'drm', driver ='drm'),
+        sysfs.Device(bus = 'host1x', name = 'drm', driver = 'drm'),
     # I2C bus
     ] + [
-        sysfs.Device(bus = 'i2c', name = '0-001a', driver ='wm8903'),
-        sysfs.Device(bus = 'i2c', name = '0-0044', driver ='isl29018'),
-        sysfs.Device(bus = 'i2c', name = '3-0034', driver ='tps6586x'),
-        sysfs.Device(bus = 'i2c', name = '3-004c', driver ='lm90'),
+        sysfs.Device(bus = 'i2c', name = '0-001a', driver = 'wm8903'),
+        sysfs.Device(bus = 'i2c', name = '0-0044', driver = 'isl29018'),
+        sysfs.Device(bus = 'i2c', name = '3-0034', driver = 'tps6586x'),
+        sysfs.Device(bus = 'i2c', name = '3-004c', driver = 'lm90'),
     ]
 
     allowlist = [
