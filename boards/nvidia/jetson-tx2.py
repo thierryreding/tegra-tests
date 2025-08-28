39fb3d43cb6821bb4d525333bb36609b4cf90cc5
Jon Hunter <jonathanh@nvidia.com>
2025-08-28 11:34:32 +0100

    boards: Allow memory-controller warnings for Tegra186
    Commit 621ce86c1e63 ("boards: Add memory-controller warnings to the
    allowed list") and commit 1df38dad130c ("boards: Update
    memory-controller warnings") added various memory-controller warnings to
    the allowed list for Tegra194 devices. These warnings are also observed
    on Tegra186 and so add these to the allowed list for Tegra186 as well.
    
    Signed-off-by: Jon Hunter <jonathanh@nvidia.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- boards/nvidia/jetson-tx2.py
+++ boards/nvidia/jetson-tx2.py
@@ -131,6 +131,18 @@ class Board(boards.Board):
         ] if Kernel().version >= Kernel.Version('6.4.0')
     ] + [
         warning for warning in [
+            r'tegra-mc 2c00000.memory-controller: sync_state\(\) pending due to 15380000.nvjpg',
+            r'tegra-mc 2c00000.memory-controller: sync_state\(\) pending due to 154c0000.nvenc',
+            r'tegra-mc 2c00000.memory-controller: sync_state\(\) pending due to 3507000.sata',
+        ] if Kernel().version >= Kernel.Version('6.4.0')
+    ] + [
+        warning for warning in [
+            r'tegra186-emc 2c60000.external-memory-controller: sync_state\(\) pending due to 15380000.nvjpg',
+            r'tegra186-emc 2c60000.external-memory-controller: sync_state\(\) pending due to 154c0000.nvenc',
+            r'tegra186-emc 2c60000.external-memory-controller: sync_state\(\) pending due to 3507000.sata',
+        ] if Kernel().version >= Kernel.Version('6.5.0')
+    ] + [
+        warning for warning in [
             r'tegra-bpmp bpmp: sync_state\(\) pending due to 3960000.cec',
             r'tegra-bpmp bpmp: sync_state\(\) pending due to 3507000.sata',
             r'tegra-bpmp bpmp: sync_state\(\) pending due to 15380000.nvjpg',
